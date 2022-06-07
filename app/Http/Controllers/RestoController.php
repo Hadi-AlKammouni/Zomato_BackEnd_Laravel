dcb40236080ba162267e5a664cf69122f1809ec6
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-06-07 21:28:13 +0300

    Implementing api to get all restuarants

--- app/Http/Controllers/RestoController.php
+++ app/Http/Controllers/RestoController.php
@@ -4,10 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Restaurant;
+
 class RestoController extends Controller{
     
     public function getAllRestos(){
-        echo "Hello World1 !";
+        // echo "Hello World1 !";
+        
+        $restos = Restaurant::all();
+        
+        return response()->json([
+            "status" => "Success",
+            "restos" => $restos
+        ], 200);
+
     }
 
 }
